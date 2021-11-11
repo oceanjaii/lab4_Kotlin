a43a2279fb0d755b9f27b03e25e1a84f75669f67
oceanjaii <windfung16@gmail.com>
2021-11-11 20:44:56 +0800

    Second

--- app/src/main/java/com/example/lab4_kotlin/MainActivity2.kt
+++ app/src/main/java/com/example/lab4_kotlin/MainActivity2.kt
@@ -11,7 +11,7 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        //將變數與 XML 元件綁定
+
         val btnSend = findViewById<Button>(R.id.btnSend)
         val edDrink = findViewById<EditText>(R.id.edDrink)
         val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
@@ -21,15 +21,15 @@ class MainActivity2 : AppCompatActivity() {
                 Toast.makeText(this, "請輸入飲料名稱",
                     Toast.LENGTH_SHORT).show()
             else {
-                //宣告 Bundle
+
                 val b = Bundle()
-                //取得 EditText 字串內容，把飲料名稱、甜度與冰塊資訊放入 Bundle
+
                 b.putString("drink", edDrink.text.toString())
                 b.putString("sugar", radioGroup.findViewById<RadioButton>
                     (radioGroup.checkedRadioButtonId).text.toString())
                 b.putString("ice", radioGroup2.findViewById<RadioButton>
                     (radioGroup2.checkedRadioButtonId).text.toString())
-                //用 Activity.RESULT_OK 標記執行狀態並記錄 Intent
+
                 setResult(Activity.RESULT_OK, Intent().putExtras(b))
                 finish()
             }
